8f474ac96cb9c6f804022cafd0047f7f9179b42c
rafaapcode <rafaap2003@gmail.com>
2024-02-22 07:19:11 -0300

    feat: finishing the integration of Clerk webhooks with Mysql Database

--- app/api/webhook/clerk/route.ts
+++ app/api/webhook/clerk/route.ts
@@ -1,6 +1,7 @@
 import { Webhook } from "svix";
 import { headers } from "next/headers";
 import { WebhookEvent } from "@clerk/nextjs/server";
+import { db } from "@/lib/db";
 
 export async function POST(req: Request) {
     const WEBHOOK_SECRET = process.env.WEBHOOK_SECRET;
@@ -38,11 +39,47 @@ export async function POST(req: Request) {
         })
     }
 
-    const { id } = evt.data;
     const eventType = evt.type;
 
-    console.log(`Webhook with and ID of ${id} and type of ${eventType}`)
-    // console.log('Webhook body:', body)
+    if (eventType === "user.created") {
+        await db.user.create({
+            data: {
+                externalUserId: payload.data.id,
+                username: payload.data.username,
+                imageUrl: payload.data.image_url,
+            }
+        });
+    }
+
+    if (eventType === 'user.updated') {
+        const currentUser = await db.user.findUnique({
+            where: {
+                externalUserId: payload.data.id
+            }
+        });
+
+        if (!currentUser) {
+            return new Response("Usuário não encontrado", { status: 404 });
+        }
+
+        await db.user.update({
+            where: {
+                externalUserId: payload.data.id
+            },
+            data: {
+                username: payload.data.username,
+                imageUrl: payload.data.image_url
+            }
+        });
+    }
+
+    if (eventType === 'user.deleted') {
+        await db.user.delete({
+            where: {
+                externalUserId: payload.data.id
+            }
+        });
+    }
 
     return new Response('', { status: 200 })
 }
