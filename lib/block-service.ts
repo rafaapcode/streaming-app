8f0cabeab21218325eb4dd2412d0c9b46e82f125
rafaapcode <rafaap2003@gmail.com>
2024-02-27 07:14:05 -0300

    feat: creating the block service

---
+++ lib/block-service.ts
@@ -0,0 +1,119 @@
+import { getSelf } from "./auth-service";
+import { db } from "./db";
+
+export const isBlockedByUser = async (id: string) => {
+    try {
+        const self = await getSelf();
+        const otherUser = await db.user.findUnique({
+            where: { id }
+        });
+
+        if (!otherUser) {
+            throw new Error("User not found");
+        }
+
+        if (otherUser.id === self.id) {
+            return false;
+        }
+
+        const exisitingBlock = await db.block.findUnique({
+            where: {
+                blockedId_blockerId: {
+                    blockerId: otherUser.id,
+                    blockedId: self.id
+                }
+            }
+        });
+
+
+        return !!exisitingBlock;
+
+    } catch (error) {
+        return false;
+    }
+};
+
+
+export const blokUser = async (id: string) => {
+    const self = await getSelf();
+
+    if (self.id === id) {
+        throw new Error("Cannot block yourself");
+    }
+
+    const otherUser = await db.user.findUnique({
+        where: { id }
+    });
+
+    if (!otherUser) {
+        throw new Error("User not found");
+    }
+
+    const existingBlock = await db.block.findUnique({
+        where: {
+            blockedId_blockerId: {
+                blockerId: self.id,
+                blockedId: otherUser.id
+            }
+        }
+    });
+
+    if (existingBlock) {
+        throw new Error("Already blocked");
+    }
+
+    const block = await db.block.create({
+        data: {
+            blockedId: otherUser.id,
+            blockerId: self.id
+        },
+        include: {
+            blocked: true
+        }
+    });
+
+
+    return block;
+};
+
+
+export const unblockUser = async (id: string) => {
+    const self = await getSelf();
+
+    if (self.id === id) {
+        throw new Error("Cannot unblock yourself");
+    }
+
+    const otherUser = await db.user.findUnique({
+        where: { id }
+    });
+
+    if (!otherUser) {
+        throw new Error("User not found");
+    }
+
+    const existingBlock = await db.block.findUnique({
+        where: {
+            blockedId_blockerId: {
+                blockerId: self.id,
+                blockedId: otherUser.id
+            }
+        }
+    });
+
+    if (!existingBlock) {
+        throw new Error("User already unblocked");
+    }
+
+    const unblock = await db.block.delete({
+        where: {
+            id: existingBlock.id
+        },
+        include: {
+            blocked: true
+        }
+    });
+
+
+    return unblock;
+};
