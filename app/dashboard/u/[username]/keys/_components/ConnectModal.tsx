1838459c773fae25f7bcafd1f982c568d67f48ed
rafaapcode <rafaap2003@gmail.com>
2024-03-03 08:39:05 -0300

    feat: creating the credentials for stream'

--- app/dashboard/u/[username]/keys/_components/ConnectModal.tsx
+++ app/dashboard/u/[username]/keys/_components/ConnectModal.tsx
@@ -1,12 +1,37 @@
 "use client";
 
+import { createIngress } from "@/actions/ingress";
 import { Alert, AlertDescription, AlertTitle } from "@/components/ui/alert";
 import { Button } from "@/components/ui/button";
 import { Dialog, DialogClose, DialogContent, DialogHeader, DialogTitle, DialogTrigger } from "@/components/ui/dialog";
 import { Select, SelectContent, SelectItem, SelectTrigger, SelectValue } from "@/components/ui/select";
+import { IngressInput } from "livekit-server-sdk";
 import { AlertTriangle } from "lucide-react";
+import { ElementRef, useRef, useState, useTransition } from "react";
+import { toast } from "sonner";
+
+
+const RTMP = String(IngressInput.RTMP_INPUT);
+const WHIP = String(IngressInput.WHIP_INPUT);
+
+type IngressType = typeof RTMP | typeof WHIP;
 
 export default function ConnectModal() {
+    const closeRef = useRef<ElementRef<"button">>(null);
+    const [isPending, startTransition] = useTransition();
+    const [ingressType, setIngressType] = useState<IngressType>(RTMP);
+
+    const onSubmit = () => {
+        startTransition(() => {
+            createIngress(parseInt(ingressType))
+                .then(() => {
+                    toast.success("Credenciais criada");
+                    closeRef.current?.click();
+                })
+                .catch(() => toast.error("Algo deu errado"))
+        });
+    };
+
     return (
         <Dialog>
             <DialogTrigger asChild>
@@ -18,13 +43,13 @@ export default function ConnectModal() {
                 <DialogHeader>
                     <DialogTitle>Gerar Conexão</DialogTitle>
                 </DialogHeader>
-                <Select>
+                <Select disabled={isPending} value={ingressType} onValueChange={(value) => setIngressType(value)} >
                     <SelectTrigger className="w-full">
-                        <SelectValue placeholder="Tipo de Ingresso"/>
+                        <SelectValue placeholder="Tipo de Ingresso" />
                     </SelectTrigger>
                     <SelectContent>
-                        <SelectItem value="RTMP">RTMP</SelectItem>
-                        <SelectItem value="WHIP">WHIP</SelectItem>
+                        <SelectItem value={RTMP}>RTMP</SelectItem>
+                        <SelectItem value={WHIP}>WHIP</SelectItem>
                     </SelectContent>
                 </Select>
                 <Alert>
@@ -35,12 +60,12 @@ export default function ConnectModal() {
                     </AlertDescription>
                 </Alert>
                 <div className="flex justify-between">
-                    <DialogClose>
+                    <DialogClose ref={closeRef} asChild>
                         <Button variant={"ghost"}>
                             Cancelar
                         </Button>
                     </DialogClose>
-                    <Button onClick={() => { }} variant={"primary"}>
+                    <Button disabled={isPending} onClick={onSubmit} variant={"primary"}>
                         Gerar
                     </Button>
                 </div>
