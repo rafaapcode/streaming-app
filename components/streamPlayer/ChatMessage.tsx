16e480cf36360389947714b3c7ba2365373949a5
rafaapcode <rafaap2003@gmail.com>
2024-03-09 15:25:13 -0300

    feat: creating the chat components

--- components/streamPlayer/ChatMessage.tsx
+++ components/streamPlayer/ChatMessage.tsx
@@ -1,19 +1,30 @@
 import { stringToColor } from "@/lib/utils";
 import { ReceivedChatMessage } from "@livekit/components-react";
+import { format } from "date-fns";
 
 interface ChatMessageProps {
     data: ReceivedChatMessage;
 }
 
-export default function ChatMessage({data}: ChatMessageProps) {
+export default function ChatMessage({ data }: ChatMessageProps) {
 
     const color = stringToColor(data.from?.name || "");
 
     return (
         <div className="flex gap-2 p-2 rounded-md hover:bg-white/5">
             <p>
-                
+                {format(data.timestamp, "HH:MM")}
             </p>
+            <div className="flex flex-wrap items-baseline gap-1 grow">
+                <p className="text-sm font-semibold whitespace-nowrap">
+                    <span className="truncate" style={{ color: color }}>
+                        {data.from?.name}
+                    </span>
+                </p>
+                <p className="text-sm break-all">
+                    {data.message}
+                </p>
+            </div>
         </div>
-    )
-}
+    );
+};
