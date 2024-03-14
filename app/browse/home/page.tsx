b42e95d3a4190f5313202f43272659cc074a693a
rafaapcode <rafaap2003@gmail.com>
2024-03-14 08:00:33 -0300

    feat: creating the home page and feed service

--- app/browse/home/page.tsx
+++ app/browse/home/page.tsx
@@ -1,5 +1,10 @@
+import { Suspense } from "react";
+import { Results, ResultsSkeleton } from "./_components/Results";
+
 export default function Page() {
-    return <div>
-        Testando
+    return <div className="h-full p-8 max-w-screen-2xl mx-auto">
+        <Suspense fallback={<ResultsSkeleton />}>
+            <Results />
+        </Suspense>
     </div>
 }
